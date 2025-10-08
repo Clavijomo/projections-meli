54b1b15be0ae1c6898b15da05109045764d77d9b
Jonathan Clavijo <jonathanclavijo0907@gmail.com>
2025-10-07 23:04:09 -0500

    [hotfix] Fix GoogleGENAI Apikey

--- src/services/generateContentGemini.ts
+++ src/services/generateContentGemini.ts
@@ -1,6 +1,10 @@
 import type { HistoricalData } from "@/types/data";
 import { GoogleGenAI } from "@google/genai";
 
+// Para este proyecto no se decidió poner la APIKEY en variable de entorno 
+// ya que no es posible usar @google/genai directamente desde un proyecto React (Frontend puro)
+// porque requiere una API Key privada y el SDK bloquea su uso en el navegador por seguridad.
+// Por cuestiones lógicas, esto claramente debe ir en variables de entorno porque se trata de información sensible.
 const AI = new GoogleGenAI({ apiKey: "AIzaSyADA-4u6bvCJkf5kfqbnE0F7ofpzPcamU0" });
 
 export async function fetchGetIA(data: HistoricalData[]) {
